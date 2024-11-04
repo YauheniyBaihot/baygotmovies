e97aea59ca07f00d984e29cfb88b419b04da18d6
Yauheniy Baihot <yauheniybaihot@gmail.com>
2024-11-04 23:01:01 +0200

    Fix translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,4 +6,6 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'lt'],
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/public/locales',
 };
