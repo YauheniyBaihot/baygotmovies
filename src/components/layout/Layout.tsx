50321959d9c804fcf8f5a0453db8895697760b47
Yauheniy Baihot <yauheniybaihot@gmail.com>
2024-11-05 20:49:07 +0200

    Fix performance issue.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import {ActionIcon, Affix, Group, Transition} from '@mantine/core';
 import {useInViewport, useWindowScroll} from '@mantine/hooks';
-import {FC, PropsWithChildren} from 'react';
+import {FC, PropsWithChildren, useMemo} from 'react';
 
 import {ContactMeButton} from '@/components/contact-me-button/ContactMeButton';
 import {Footer} from '@/components/footer/Footer';
@@ -17,23 +17,27 @@ export const Layout: FC<PropsWithChildren<LayoutProps>> = ({sections, children})
   const [scroll, scrollTo] = useWindowScroll();
   const {ref, inViewport} = useInViewport();
 
-  return (
-    <div className={classes.container}>
-      {children}
-      <Footer ref={ref} sections={sections} />
+  const showAffix = scroll.y > 0 && !inViewport;
 
-      <Affix position={{bottom: 'var(--app-padding-y)', right: 'var(--app-padding-x)'}}>
-        <Transition transition="slide-up" mounted={scroll.y > 0 && !inViewport}>
-          {transitionStyles => (
-            <Group style={transitionStyles}>
-              <ContactMeButton />
-              <ActionIcon className={classes.iconButton} variant="main" onClick={() => scrollTo({y: 0})}>
-                <IconUp />
-              </ActionIcon>
-            </Group>
-          )}
-        </Transition>
-      </Affix>
-    </div>
-  );
+  return useMemo(() => {
+    return (
+      <div className={classes.container}>
+        {children}
+        <Footer ref={ref} sections={sections} />
+
+        <Affix position={{bottom: 'var(--app-padding-y)', right: 'var(--app-padding-x)'}}>
+          <Transition transition="slide-up" mounted={showAffix}>
+            {transitionStyles => (
+              <Group style={transitionStyles}>
+                <ContactMeButton />
+                <ActionIcon className={classes.iconButton} variant="main" onClick={() => scrollTo({y: 0})}>
+                  <IconUp />
+                </ActionIcon>
+              </Group>
+            )}
+          </Transition>
+        </Affix>
+      </div>
+    );
+  }, [children, ref, sections, showAffix, scrollTo]);
 };
